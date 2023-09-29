9e3dae351aab2decb8f7ba7e23e4264bb3b82ad7
Firas <VaselineCoveredCat@users.noreply.github.com>
2023-09-29 20:26:24 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,21 @@ import autoprefixer from "autoprefixer";
 import dartsass from "sass";
 import stylelint from "stylelint";
 import postcss from "gulp-postcss";
+import gulpbabel from "gulp-babel";
 import gulpsass from "gulp-sass";
+import gulptype from "gulp-typescript";
 
 const sass = gulpsass(dartsass);
+const ts = gulptype.createProject("tsconfig.json");
 
-function compilesass() {
+export function compiletypescript() {
+  return gulp.src("src/typescripts/index.ts").pipe(ts()).pipe(gulpbabel()).pipe(gulp.dest("scripts"));
+}
+
+export default function compilesass() {
   return gulp
-    .src("sass/index.scss")
+    .src("src/sass/index.scss")
     .pipe(sass())
     .pipe(postcss([autoprefixer(), stylelint({fix: true})]))
     .pipe(gulp.dest("styles"));
 }
-
-export default compilesass;
