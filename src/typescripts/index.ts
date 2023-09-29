68a90b61ef43912130fc100b0902d9bf8b64458b
Firas <VaselineCoveredCat@users.noreply.github.com>
2023-09-29 20:27:26 +0700

    Create index.ts

---
+++ src/typescripts/index.ts
@@ -0,0 +1,66 @@
+const inputs: NodeListOf<Element> = document.querySelectorAll(".js-check");
+const calc: HTMLButtonElement = document.querySelector("button")!;
+
+function checkInput(el: HTMLInputElement): void {
+  if (/^([1-9]\d*)$/.test(el.value)) {
+    el.nextElementSibling!.setAttribute("hidden", "");
+    el.setAttribute("aria-invalid", "false");
+    el.classList.toggle("valid", true);
+    el.classList.toggle("invalid", false);
+  } else {
+    el.nextElementSibling!.removeAttribute("hidden");
+    el.setAttribute("aria-invalid", "true");
+    el.classList.toggle("valid", false);
+    el.classList.toggle("invalid", true);
+  }
+}
+
+inputs.forEach((item: Element): void => {
+  item.addEventListener("keydown", (event: Event): void => {
+    if (
+      !"0123456789".includes((event as KeyboardEvent).key) &&
+      !["ArrowLeft", "ArrowRight", "Backspace", "Delete", "Enter", "Tab"].includes((event as KeyboardEvent).key)
+    ) {
+      event.preventDefault();
+    }
+  });
+
+  item.addEventListener("blur", (event: Event): void => {
+    checkInput(event.target as HTMLInputElement);
+  });
+});
+
+calc.addEventListener("click", (event: MouseEvent): void => {
+  event.preventDefault();
+
+  inputs.forEach((item: Element): void => {
+    checkInput(item as HTMLInputElement);
+  });
+
+  if (document.querySelector("input:invalid")) {
+    (document.querySelector("input:invalid")! as HTMLElement).focus();
+  } else {
+    const goal: number = +(document.querySelector("#goal")! as HTMLInputElement).value;
+    const income: number = +(document.querySelector("#income")! as HTMLInputElement).value;
+    const deadline: number = +(document.querySelector("#deadline")! as HTMLInputElement).value;
+
+    const offsetLow: number = goal - goal * 0.02;
+    const offsetHigh: number = goal + goal * 0.02;
+
+    let max: number = 100;
+    let min: number = 0;
+    let mid: number = 0;
+    let bank: number = 0;
+
+    if (income * deadline > goal) {
+      console.log();
+    } else {
+      document.querySelector(".js-result")!.textContent = "Tidak mungkin.";
+    }
+
+    inputs.forEach((item: Element): void => {
+      (item as HTMLInputElement).value = "";
+      (item as HTMLInputElement).classList.toggle("valid", false);
+    });
+  }
+});
